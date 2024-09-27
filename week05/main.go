75717f5eeab99210f738937e64fce675538941d4
Jeong8537 <jeong85373546@gmail.com>
2024-09-27 18:24:45 +0900

    v0.2 IO | types

---
+++ week05/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt" // == c lang, #include <stdio.h>
+
+func main() {
+	// var i int
+	// i = 55
+
+	//var i int = 55
+	var f float32 = 1.92
+
+	i := 55 // variable inference
+
+	fmt.Println("f is", f)
+	fmt.Println("i is", i)
+	fmt.Print("i is ", i, "\n")
+	fmt.Println("i is", i)
+	fmt.Printf("i is %d\n", i)
+}
