35aafd7979166201fd358c345e3aacc5b5841ce4
Jeong8537 <jeong85373546@gmail.com>
2024-10-11 16:57:28 +0900

    v0.8 | Replace

--- week07/main.go
+++ week07/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
+	var hello string = "He##o, wor#d!"
+	helloFixed := strings.NewReplacer("#", "l")
+	fmt.Println(hello)
+	fmt.Println(helloFixed.Replace(hello))
 }
