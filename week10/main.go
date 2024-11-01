73d0a2cc2266f4d45c7543237b6d751335304535
Jeong8537 <jeong85373546@gmail.com>
2024-11-01 18:26:19 +0900

    v2.9 | Prime Number Function

--- week10/main.go
+++ week10/main.go
@@ -9,8 +9,30 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		i := 3
+
+		for i*i <= n {
+			if n%i == 0 {
+				return false
+				// isPrime = false
+				// break
+			}
+			fmt.Printf("%d ", i)
+			i = i + 2
+		}
+	}
+	return true
+}
+
 func main() {
-	// fmt.Printf("%f\n", math.Sqrt(19.0)) // root
 	fmt.Print("Input Number : ")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
@@ -25,32 +47,7 @@ func main() {
 		log.Fatal()
 	}
 
-	var isPrime bool = true
-	if n <= 1 {
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 { // Among even number, only 2 is Prime Number
-		isPrime = false
-	} else {
-		i := 3
-		// for i < n {
-		// for i < int(math.Sqrt(float64(n))) { // Error
-		// for i <= int(math.Sqrt(float64(n))) { // Same as current code
-
-		//for float64(i) < math.Sqrt(float64(n)) { // Same as current code
-		for i*i <= n {
-			if n%i == 0 {
-				isPrime = false
-				break // first divisor, loop break
-			}
-			fmt.Printf("%d ", i)
-			// i++
-			i = i + 2
-		}
-	}
-
-	if isPrime {
+	if isPrime(n) {
 		fmt.Printf("%d is a Prime Number.", n)
 	} else {
 		fmt.Printf("%d not a Prime Number.", n)
