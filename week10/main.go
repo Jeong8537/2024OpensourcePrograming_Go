f595b3ab5d80bdce834c58cfdb4e982f76c44420
Jeong8537 <jeong85373546@gmail.com>
2024-11-01 18:00:15 +0900

    v2.6 | Prime Number

--- week10/main.go
+++ week10/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	// fmt.Printf("%f\n", math.Sqrt(19.0)) // root
 	fmt.Print("Input Number : ")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
@@ -26,11 +28,14 @@ func main() {
 
 	var isPrime bool = true
 	if n <= 1 {
-
 		isPrime = false
 	} else {
 		i := 2
-		for i < n {
+		//for i < n {
+		// for i < int(math.Sqrt(float64(n))) { // Error
+		// for i <= int(math.Sqrt(float64(n))) { == Same as current code
+
+		for float64(i) < math.Sqrt(float64(n)) {
 			if n%i == 0 {
 				isPrime = false
 				break // first divisor, loop break
