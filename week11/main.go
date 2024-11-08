44e2e98eba43791e125340a9971413b192e8660d
Jeong8537 <jeong85373546@gmail.com>
2024-11-08 18:25:20 +0900

    v3.7 | package control

--- week11/main.go
+++ week11/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"week11/keyboard"
+	// "github.com/headfirstgo/keyboard" // go get github.com/headfirstgo/keyboard
 )
 
 func main() {
-	n, _ := keyboard.GetFloat()
+	// n, _ := keyboard.GetFloat()
+	n, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(n)
 }
